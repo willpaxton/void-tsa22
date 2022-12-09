2d41da1ccf438bb0b3c640335dc907fab794d8f0
Will Paxton <43458264+willpaxton@users.noreply.github.com>
2022-12-09 12:38:04 -0500

    corrected spelling

--- js/alert.js
+++ js/alert.js
@@ -1,5 +1,4 @@
 function gameAlert() {
-    alert("Developers Note: On the production site, this linked to an external game; however, that was not my worked so you're seeing this instead. :)")
-    console.log("teehee moment")
+    alert("Developers Note: On the production site, this linked to an external game; however, that was not my own work so you're seeing this instead. :)")
 }
 
